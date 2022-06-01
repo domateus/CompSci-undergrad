bfd12377f21212940eded44ddc1897299b0247d7
Mateus Antonio Franceschina <mateus.franceschina@pampa.compasso>
2022-05-31 21:03:32 -0300

    explains code

--- nws/authClient.mjs
+++ nws/authClient.mjs
@@ -3,7 +3,6 @@ import {
   generateKeyPairSync,
   privateDecrypt,
   privateEncrypt,
-  publicDecrypt,
 } from "crypto";
 import fs from "fs";
 import fetch from "node-fetch";
@@ -12,6 +11,7 @@ const passphrase = "pudim";
 const format = "pem";
 const server = "http://localhost:3000/";
 
+// gera chaves assimetricas
 const generateAsymmetricKeys = () => {
   return Object.freeze(
     generateKeyPairSync("rsa", {
@@ -30,13 +30,17 @@ const generateAsymmetricKeys = () => {
   );
 };
 
-const { privateKey, publicKey } = generateAsymmetricKeys();
+const { privateKey, publicKey } = generateAsymmetricKeys(); // constroi chave assimetrica
+// gera um usuário aleatório
 const getUser = () => ({
   clientPublicKey: publicKey,
   username: `Mateus#${parseInt(Math.random() * 100)}`,
 });
+
 let serverPublicKey;
 let serverSymmetricKey;
+
+// função auxiliar para fazer requisições
 const req = async (route, body) => {
   const res = await fetch(server + route, {
     method: "POST",
@@ -45,16 +49,21 @@ const req = async (route, body) => {
   });
   return await res.json();
 };
+
+// envia chave publica ao servidor e recebe a chave publica do servidor
 const greetings = async (me) => {
   const data = await req("greetings", me);
+  // descriptografa a resposta do servidor com a chave privada do cliente
   const text = privateDecrypt(
     { key: privateKey, passphrase },
     Buffer.from(data.greeting)
   );
+  // salva chave publica do servidor
   serverPublicKey = data.publicKey;
   console.log(text.toString("utf-8"));
 };
 
+// recebe chave simetrica do servidor
 const hotStuff = async (me) => {
   const data = await req("hot-stuff", {
     username: me.username,
@@ -64,22 +73,33 @@ const hotStuff = async (me) => {
     ),
   });
 
-  serverSymmetricKey = publicDecrypt(serverPublicKey, Buffer.from(data.answer));
+  // descriptografa a chave simetrica do servidor com a chave privada do cliente
+  serverSymmetricKey = privateDecrypt(
+    { key: privateKey, passphrase },
+    Buffer.from(data.answer)
+  );
   console.log(
     "hotstuff, symmetric key is: ",
     serverSymmetricKey.toString("hex")
   );
 };
 
-const secureChannel = async (fail = false) => {
+// usa canal de comunicação segura
+const secureChannel = async () => {
+  // le arquivo de texto local
   const text = fs.readFileSync("camoes.txt", "utf-8");
   const lines = text.split(/\r?\n/);
   try {
+    // para cada linha envia uma requisição para o servidor
     for (let i = 0; i < lines.length; i++) {
       const hmac = createHmac("sha256", serverSymmetricKey);
       let line = lines[i];
       hmac.update(line);
+      // gera um hash com a chave simetrica do texto lido
       const hash = hmac.digest("hex");
+      // há uma chance de 15% de erro, só para fins ilustrativos
+      // essa condicional modifica o texto enviado
+      // então o hash gerado pelo servidor é diferente do hash gerado pelo cliente
       if (Math.random() < 0.15) line = "opa";
       const data = await req("secure-channel", { text: line, hash });
       console.log(data, i + 1);
@@ -88,11 +108,19 @@ const secureChannel = async (fail = false) => {
 };
 
 const go = async () => {
+  // busca o usuario
   const me = getUser();
   console.log("me", me.username);
+  // faz os cumprimentos com o servidor, aqui ocorre a troca de chaves publicas
   await greetings(me);
+  // cliente e servidor usam a criptografia assimetrica para trocar chave simetrica
+  // o servidor encripta a chave simetrica com a chave publica do cliente
+  // e o cliente então usa sua chave privada para descriptografar a chave simétrica
   await hotStuff(me);
+  // ao ter garantido a autenticidade do cliente, basta que o canal de comunicação
+  // garanta a integridade da mensagem, então usando a criptografia simétrica
   await secureChannel();
 };
 
+// inicia o programa
 await go();
--- nws/authServer.js
+++ nws/authServer.js
@@ -1,7 +1,6 @@
 const {
   privateEncrypt,
   publicDecrypt,
-  privateDecrypt,
   generateKeyPairSync,
   publicEncrypt,
   scryptSync,
@@ -12,6 +11,8 @@ const express = require("express");
 const passphrase = "pudim";
 const format = "pem";
 const users = [];
+
+// gera chave assimetrica
 const generateAsymmetricKeys = () => {
   return Object.freeze(
     generateKeyPairSync("rsa", {
@@ -30,49 +31,58 @@ const generateAsymmetricKeys = () => {
   );
 };
 
-const generateSymmetricKey = () => {
-  return scryptSync(passphrase, "salt", 64, { encoding: "hex" });
-};
+// gera chave simetrica
+const generateSymmetricKey = () =>
+  scryptSync(passphrase, "salt", 64, { encoding: "hex" });
 
+// salva usuário e criptografa uma mensagem com a chave publica do cliente
 const buildGreetingWithPublicKey = ({ clientPublicKey, username }) => {
   users.push({ clientPublicKey, username });
   return publicEncrypt(clientPublicKey, Buffer.from("hi"));
 };
 
-const { privateKey, publicKey } = generateAsymmetricKeys();
-const symmetricKey = generateSymmetricKey();
+const { privateKey, publicKey } = generateAsymmetricKeys(); // constroi chave assimetrica
+const symmetricKey = generateSymmetricKey(); // constroi chave simetrica
+
+// constroi servidor
 const app = express();
 app.use(express.json());
 
+// rota para recerber chave publica do cliente
+// responde com a chave publica do servidor
 app.post("/greetings", (req, res) => {
   const greeting = buildGreetingWithPublicKey(req.body);
   console.log([req.body.username]);
   res.json({ greeting, publicKey });
 });
 
+// rota para encaminhar a chave simetrica
 app.post("/hot-stuff", (req, res) => {
   const { text, username } = req.body;
+  // busca qual cliente esta mandando mensagem
   const clientPublicKey = users.find(
     (u) => u.username === username
   ).clientPublicKey;
+  //descriptograva a mensagem enviada pelo cliente com a sua chave publica
   const inNeed = publicDecrypt(clientPublicKey, Buffer.from(text)).toString(
     "utf-8"
   );
   let answer = "bye";
+  // se a mensagem do cliente for "symmetricKey" responde com a chave simetrica
   if (inNeed === "symmetricKey") {
-    answer = privateEncrypt(
-      { key: privateKey, passphrase },
-      Buffer.from(symmetricKey)
-    );
+    answer = publicEncrypt(clientPublicKey, Buffer.from(symmetricKey));
   }
   res.json({ answer });
 });
 
+// canal de comunicação com criptografia simetrica
 app.post("/secure-channel", (req, res) => {
   const { text, hash } = req.body;
   const hmac = createHmac("sha256", symmetricKey);
+  // faz o hash do texto recebido do cliente
   hmac.update(text);
   let answer = "compromised";
+  // se o hash for igual ao hash recebido pelo cliente responde com "ok"
   if (hmac.digest("hex") == hash) {
     console.log(text);
     answer = "alles gut";
